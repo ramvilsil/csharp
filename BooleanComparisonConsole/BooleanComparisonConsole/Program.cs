18f341f85ca007e19401f3d0e504fc872ad6675c
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-09 20:01:27 -0800

    Boolean Comparison #1

---
+++ BooleanComparisonConsole/BooleanComparisonConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿//Declares namespace as "BooleanComparison"
+namespace BooleanComparison
+{
+    //Declares the class "Program"
+    class Program
+    {
+        //Defines the program entry point
+        static void Main(string[] args)
+        {
+            //Declares the variable "number" an integer assigned a value of 1
+            int number = 1;
+
+            //Performs block of code while number is less than 5
+            do
+            {
+                //Writes a line displaying the number
+                Console.WriteLine(number);
+                //Increment number by 1 each line
+                number++;
+            }
+            while (number < 5);
+
+            //While the "number" variable value is less than or equal to 100 write line
+            //displaying the number and increment the number by 10 each time
+            while (number <= 100)
+            {
+                Console.WriteLine(number);
+                //Increment number by 10 each line
+                number += 10;
+            }
+        }
+            
+    }
+}
