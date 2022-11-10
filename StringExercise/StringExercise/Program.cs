fe13975fce0dcad88205b680bcd37478fdadf4fc
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-10 11:01:10 -0800

    String Exercise #1

---
+++ StringExercise/StringExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//Program calls the "System.Text" namespace
+using System.Text;
+
+//Declares the name space as "StringExercise"
+namespace StringExercise
+{
+    //Declares the class as "Program:
+    class Program
+    {
+        //Program entry point
+        static void Main(string[] args)
+        {
+            //Creates a string through concatenation and assigns it to the variable "numbers"
+            string numbers = ("One " + "Two " + "Three ");
+
+            //Writes the string assigned to "numbers" on the console
+            Console.WriteLine(numbers.ToUpper());
+
+            //Creates a string builder with the variable name "sb"
+            StringBuilder sb = new StringBuilder();
+
+            //Appends lines to the string builder "sb"
+            sb.AppendLine("--------");
+            sb.AppendLine("Welcome.");
+            sb.AppendLine("This program was made for educational purposes.");
+            sb.AppendLine("The date today is 11/10/2022.");
+            sb.AppendLine("New Year's Eve will be in 51 days.");
+
+            //Writes "sb" Stringbuilder on the console
+            Console.WriteLine(sb);
+        }
+
+    }
+}
