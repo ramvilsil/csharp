f09e1d89c98fad2dd93e9944393ab7ca8420f8fc
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-10 17:02:59 -0800

    Arrays Exercise #2

---
+++ ArrayIterationsConsoleExcercise/ArrayIterationsConsoleExcercise/Program.cs
@@ -0,0 +1,97 @@
+﻿
+
+namespace ArrayIterationsExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //Part 1
+
+            string[] stringsArray = { "1: ", "2: ", "3: ", "4: ", "5: " };
+
+            Console.WriteLine("Enter a word: ");
+
+            string word = Console.ReadLine() + "\n";
+
+            for (int i = 0; i < stringsArray.Length; i++)
+            {
+                stringsArray[i] = stringsArray[i] + word;
+            }
+
+            for (int i = 0; i < stringsArray.Length; i++)
+            {
+                Console.WriteLine(stringsArray[i]);
+            }
+
+
+            //Part 2
+
+            int num = 0;
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine(num + "\n");
+               
+                num++;
+
+                if (num == 10)
+                {
+                    running = false;
+                }
+            }
+
+
+            //Part 3
+
+            num = 40;
+
+            string outputText = "Hello World! ";
+
+            running = true;
+
+            while (running)
+            {
+                Console.WriteLine(outputText + num + "\n");
+
+                num--;
+
+                if (num < 20)
+                {
+                    running = false;
+                }
+            }
+
+
+            num = 100;
+
+            outputText = "Goodbye World. ";
+
+            running = true;
+
+            while (running)
+            {
+                Console.WriteLine(outputText + num + "\n");
+
+                num -= 20;
+
+                if (num <= 20)
+                {
+                    running = false;
+                }
+            }
+
+
+            //Part 4
+
+
+
+            
+
+        }
+
+    }
+}
--- ArraysConsoleExercise/ArraysConsoleExercise/Program.cs
+++ ArraysConsoleExercise/ArraysConsoleExercise/Program.cs
@@ -22,9 +22,34 @@ namespace ArraysExercise
             Console.WriteLine("Enter a number from 0-4: ");
             int userInput = Convert.ToInt16(Console.ReadLine());
 
-            //Writes a line on the console displaying the corresponding output
-            Console.WriteLine("Output: " + stringArray[userInput] + "\n");
+            //Creates boolean to check if the user enters an invalid number
+            bool inArray = false;
 
+            //Iterates through every index in the "stringArray"
+            //Boolean "inArray" will be assigned true if a valid number is entered
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                //Compares each index in the array to the user input
+                if (i == userInput)
+                {
+                    //Writes the line on the console
+                    Console.WriteLine("Output: " + stringArray[userInput] + "\n");
+                    
+                    //If the program reaches this point "inArray" becomes true
+                    //Prevents error message from being thrown
+                    inArray = true;
+
+                }
+
+            //Checks if the "inArray" boolean value is false
+            //i.e. the value will be false if the program never found a match between the array index and the user input
+            }
+            if (inArray == false)
+            {
+                //Writes error message
+                Console.WriteLine("You entered an invalid number");
+
+            }
 
 
             //Integers array
@@ -41,8 +66,80 @@ namespace ArraysExercise
             Console.WriteLine("Enter a number from 0-4: ");
             userInput = Convert.ToInt16(Console.ReadLine());
 
-            //Writes a line on the console displaying the corresponding output
-            Console.WriteLine("Output: " + numArray[userInput]);
+            //Reasigns boolean to check if the user enters an invalid number
+            inArray = false;
+
+
+            //Iterates through every index in the "numArray"
+            //Boolean "inArray" will be assigned true if a valid number is entered
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                //Compares each index in the array to the user input
+                if (i == userInput)
+                {
+                    //Writes the line on the console
+                    Console.WriteLine("Output: " + numArray[userInput]);
+
+                    //If the program reaches this point "inArray" becomes true
+                    //Prevents error message from being thrown
+                    inArray = true;
+
+                }
+
+            //Checks if the "inArray" boolean value is false
+            //i.e. the value will be false if the program never found a match between the array index and the user input
+            }
+            if (inArray == false)
+            {
+                //Writes error message
+                Console.WriteLine("You entered an invalid number");
+
+            }
+
+            //Creates string list "namesList"
+            List<string> namesList = new List<string>();
+
+            //Adds names to the list in the form of strings
+            namesList.Add("Jake");
+            namesList.Add("Carl");
+            namesList.Add("Elizabeth");
+            namesList.Add("Kate");
+            namesList.Add("Bob");
+
+            //Requests the user input and assigns it to the variable "userInput"
+            Console.WriteLine("Enter a number from 0-4: ");
+            userInput = Convert.ToInt16(Console.ReadLine());
+
+
+            //Creates boolean to check if the user enters an invalid number
+            bool inList = false;
+
+            //Iterates through every index in the "namesList"
+            //Boolean "inList" will be assigned true if a valid number is entered
+            for (int i = 0; i < namesList.Count; i++)
+            {
+                if (i == userInput)
+                {
+                    //Writes the line on the console
+                    Console.WriteLine("The person's name at this index is " + namesList[userInput] + ".");
+
+                    //If the program reaches this point "inList" becomes true
+                    //Prevents error message from being thrown
+                    inList = true;
+
+                }
+
+            }
+
+            //Checks if the "inList" boolean value is false
+            //i.e. the value will be false if the program never found a match between the array index and the user input
+            if (inList == false)
+            {
+                //Writes error message
+                Console.WriteLine("You entered an invalid number");
+
+            }
+
 
         }
     }
