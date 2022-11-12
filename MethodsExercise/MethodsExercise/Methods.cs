a6a35286c00f2c19ac1596dfbdf5fbf63c6be95f
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-12 14:26:04 -0800

    Methods Exercise #1

---
+++ MethodsExercise/MethodsExercise/Methods.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsExercise
+{
+    //Declares the public class "Methods"
+    public class Methods
+    {
+        //Creates method "Method"
+        public static int Method(int intNum)
+        {
+            //Increments "intNum" value by 10
+            intNum += 10;
+
+            //Returns  "intNum" as an int
+            return intNum;
+        }
+
+        //Uses the same method previously used "Method" - Method overloading
+        public static int Method(double doubleNum)
+        {
+            //Multiplies "doubleNum" value by 5
+            doubleNum *= 5;
+
+            //Creates new int "nonDoubleNum" from "doubleNum" converted to an int
+            int nonDoubleNum = Convert.ToInt16(doubleNum);
+
+            //Returns "nonDoubleNum" as an int
+            return nonDoubleNum;
+        }
+
+        //Uses the same method previously used "Method" - Method overloading
+        public static int Method(string stringNum)
+        {
+            //Creates new int "nonStringNum" from "stringNum" as an int
+            int nonStringNum = Int16.Parse(stringNum);
+
+            //Divides "nonStringNum" value by 2
+            nonStringNum /= 2;
+
+            //Returns "nonStringNum" as an int
+            return nonStringNum;
+        }
+
+    }
+}
---
+++ MethodsExercise/MethodsExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace methodsExercise
+{
+    //Declares "Program" class
+    class Program
+    {
+        //Program entry point
+        static void Main()
+        {
+            //Requests user input into the console
+            Console.WriteLine("Enter a whole number to increment by 10:");
+            //Converts the value to an int and assigns it to int "intNum"
+            int intNum = Convert.ToInt16(Console.ReadLine());
+
+            //Writes line on the console displaying the method "Method" output using "intNum" as a parameter
+            Console.WriteLine(Methods.Method(intNum));
+
+
+            //Requests user input into the console
+            Console.WriteLine("Enter a decimal number to multiply by 5:");
+            //Converts the value to a double and assigns it to double "doubleNum"
+            double doubleNum = Convert.ToDouble(Console.ReadLine());
+
+            //Writes line on the console displaying the method "Method" output using "doubleNum" as a parameter
+            Console.WriteLine(Methods.Method(doubleNum) + " (displayed as a whole number)");
+
+
+            //Requests user input into the console
+            Console.WriteLine("Enter a whole number to divide by 2:");
+            //Assigns the value to string "stringNum"
+            string stringNum = Console.ReadLine();
+
+            //Writes line on the console displaying the method "Method" output using "stringNum" as a parameter
+            Console.WriteLine(Methods.Method(stringNum));
+        }
+    }
+}
