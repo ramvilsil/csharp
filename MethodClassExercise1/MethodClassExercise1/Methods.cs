7ad7653a3dbb2117439893687eaf4c1859ab33a4
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-12 15:35:58 -0800

    Method Class Exercise 1 #1

---
+++ MethodClassExercise1/MethodClassExercise1/Methods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodClassExercise1
+
+{
+    //Declares the public class "Methods"
+    public class Methods
+    {
+        //Creates method "Method" with 2 parameters
+        public static void Method(int num1, int num2)
+        {
+            //Increments parameter "num1" by 1
+            num1++;
+
+            //Writes parameter "num2" on the console
+            Console.WriteLine(num2);
+
+        }
+
+    }
+}
---
+++ MethodClassExercise1/MethodClassExercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace methodClassExercise1
+{
+    //Declares "Program" class
+    class Program
+    {
+        //Program entry point
+        static void Main()
+        {
+            //Defines "num1" and "num2" int variables
+            int num1 = 0;
+            int num2 = 0;
+
+            //Creates an instance of the "Methods" class called "myMethod"
+            Methods myMethod = new Methods();
+
+            //Calls method "Method" from the "Methods" class and uses 2 int values as the parameters
+            Methods.Method(1, 1);
+
+            //Calls method "Method" from the "Methods" class and uses 2 predefined int variables as the parameters
+            Methods.Method(num1, num2);
+
+
+        }
+    }
+}
