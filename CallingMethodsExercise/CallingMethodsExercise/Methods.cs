027b1419329c185fc1673cdccbeef74c15312298
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-11 23:10:42 -0800

    Calling Methods #1

---
+++ CallingMethodsExercise/CallingMethodsExercise/Methods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsExercise
+{
+    //Defines class "Methods"
+    public class Methods
+    {
+        //1st Method
+        //Creates method that takes the int variable "userInput" as a parameter
+        public static int Method1(int userInput)
+        {
+            //Performs math operation on "userInput" - adds 1
+            userInput++;
+
+            //Returns "userInput" argument
+            return userInput;
+        }
+
+        //2nd Method
+        //Creates method that takes the int variable "userInput" as a parameter
+        public static int Method2(int userInput)
+        {
+            //Performs math operation on "userInput" - subtracts 1
+            userInput--;
+
+            //Returns "userInput" argument
+            return userInput;
+        }
+
+        //3rd Method
+        //Creates method that takes the int variable "userInput" as a parameter
+        public static int Method3(int userInput)
+        {
+            //Performs math operation on "userInput" - multiplies by 2
+            userInput *= 2;
+
+            //Returns "userInput" argument
+            return userInput;
+        }
+    }
+}
---
+++ CallingMethodsExercise/CallingMethodsExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using CallingMethodsExercise;
+
+namespace callingMethods
+{
+    //Defines class "Program"
+    class Program
+    {
+        //Program entry point
+        static void Main()
+        {
+            //Requests input from the user and assigns it to int variable "userInput"
+            Console.WriteLine("Enter a number");
+            int userInput = Convert.ToInt16(Console.ReadLine());
+
+            //Writes a line on the console displaying the math operation undertaken by the method
+            Console.WriteLine(userInput + " + 1 = " + Methods.Method1(userInput));
+
+            Console.WriteLine(userInput + " - 1 = " + Methods.Method2(userInput));
+
+            Console.WriteLine(userInput + " x 2 = " + Methods.Method3(userInput));
+
+
+
+        }
+
+
+
+    }
+}
