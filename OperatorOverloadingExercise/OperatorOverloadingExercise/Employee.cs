fa43d3083460fa830c3444fee2c12b2a71d6e3f9
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-14 14:24:14 -0800

    Operator Overloading Exercise #1

---
+++ OperatorOverloadingExercise/OperatorOverloadingExercise/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Assigns namespace "OperatorOverloadingExercise"
+namespace OperatorOverloadingExercise
+{
+    //Creates class "Employee"
+    public class Employee
+    {
+        //Creates int property "Id"
+        public int Id { get; set; }
+
+        //Creates string property "FirstName"
+        public string FirstName { get; set; }
+
+        //Creates string property "FirstName"
+        public string LastName { get; set; }    
+
+        //Creates == operator overload method using the "Employee" objects
+        public static string operator== (Employee employee, Employee employee2)
+        {
+            //If the object's "Id" properties are equal then a message saying so will be returned
+            if (employee.Id == employee2.Id)
+            {
+                return "The employees have the same id";   
+            }
+
+            //If the object's "Id" properties are not found to be equal then a message saying so will be returned
+            else
+            {
+                return "The employees don't have the same id";
+
+            }
+
+        }
+
+        //Creates != operator overload method using the "Employee" objects
+        //As comparison operators must be overloaded in pairs
+        public static string operator!= (Employee employee, Employee employee2)
+        {
+            //If the object's "Id" properties are not equal then a message saying so will be returned
+            if (employee.Id != employee2.Id)
+            {
+                return "The employees don't have the same id";
+            }
+
+            //If the object's "Id" properties are not found to be not equal then a message saying so will be returned
+            else
+            {
+                return "The employees have the same id";
+
+            }
+        }
+
+    }
+}
---
+++ OperatorOverloadingExercise/OperatorOverloadingExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿
+//Assigns namespace "OperatorOverloadingExercise"
+namespace OperatorOverloadingExercise
+{
+    //Creates class "Program"
+    class Program
+    {
+        //Program entry point
+        static void Main()
+        {
+
+
+            //Creates "Employee" class objects
+            Employee employee = new Employee();
+            Employee employee2 = new Employee();
+
+            //Defines "Id" properties of the "Employee" objects using the user's input
+            Console.WriteLine("Enter an id for Employee 1 (must be a whole number)");
+            employee.Id = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter an id for Employee 2 (must be a whole number)");
+            employee2.Id = Convert.ToInt16(Console.ReadLine());
+
+            //Defines "FirstName" and "LastName" properties of the "Employee" objects
+            employee.FirstName = "Test1";
+            employee2.FirstName = "Test2";
+
+            employee.LastName = "Sample1";
+            employee2.LastName = "Sample2";
+
+            //Writes line on the console using operator overload method return value
+            Console.WriteLine(employee == employee2);
+
+        }
+    }
+}
