0ad33dca7ff04f940a28389679cc72f4647ccabd
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-15 15:18:45 -0800

    Structs Exercise #1

---
+++ StructsExercise/StructsExercise/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+//Declares namespace "StructsExercise"
+namespace StructsExercise
+{
+    //Creates class "Program"
+    class Program
+    {
+        //Program entry point
+        static void Main()
+        {
+            //Creates instance "number" of struct "Number"
+            Number number = new Number();
+
+            //Assigns value to "Amount" property value of "number"
+            number.Amount = 10.0m;
+
+            //Writes line displaying "Amount" property value of "number"
+            Console.WriteLine(number.Amount);
+        }
+    }
+        
+
+}
---
+++ StructsExercise/StructsExercise/Struct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Declares namespace "StructsExercise"
+namespace StructsExercise
+{
+    //Creates struct "Number"
+    public struct Number
+    {
+        //Creates decimal property "Amount"
+        public decimal Amount { get; set; }
+    }
+}
