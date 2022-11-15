804357fe81ee068d74068f28706c5cac96a40ea3
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-14 16:18:41 -0800

    Parsing Enums Exercise #1

---
+++ ParsingEnumsExercise/ParsingEnumsExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//Declares namespace "ParsingEnumsExercise"
+namespace ParsingEnumsExercise
+{
+    //Creates class "Program"
+    class Program
+    {
+        //Program entry point
+        static void Main()
+        {
+            //Writes line on the console requesting user input
+            Console.WriteLine("Enter the current day of the week:");
+
+            //Try block
+            try
+            {
+                //Takes user input as a string, parses it to a "WeekDays" enum element
+                //User input must correspond to an element in the "WeekDays" enum
+                WeekDays userInput = (WeekDays)Enum.Parse(typeof(WeekDays), Console.ReadLine(), true);
+
+                //Writes line on the console upon valid user input
+                Console.WriteLine("Success.");
+            }
+            //Catch block
+            catch (Exception)
+            {
+                //Writes line on the console upon invalid user input
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+
+        }
+
+    }
+}
---
+++ ParsingEnumsExercise/ParsingEnumsExercise/WeekDays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Declares namespace "ParsingEnumsExercise"
+namespace ParsingEnumsExercise
+{
+    //Creates enum "WeekDays"
+    public enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday                  
+    }
+}
