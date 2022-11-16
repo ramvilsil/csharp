25f847aedba3db87c02164b652a45bd910141487
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-15 21:08:02 -0800

    User Input Exercise #1

---
+++ UserInputExercise/UserInputExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+//Declares namespace "UserInputExercise"
+namespace UserInputExercise
+{
+    //Creates class "Program"
+    class Program
+    {
+        //Program entry point
+        static void Main()
+        {
+            //Writes line on console requesting user input and assigns it to "userInput"s
+            Console.WriteLine("Enter a number: ");
+            string userInput = Console.ReadLine();
+
+            //Creates file "filename.txt" and writes the content of "userInput on it
+            File.WriteAllText("filename.txt", userInput);
+
+            //Read the contents of "filename.txt" and assigns it to "readText"
+            string readText = File.ReadAllText("filename.txt");  
+            //Writes "readText" on the console
+            Console.WriteLine(readText);
+        }
+            
+
+    }
+}
