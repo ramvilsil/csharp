c26368846c9f9cd57e0096d8a4a842a1fb89bb6f
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-16 21:08:24 -0800

    Constructor Exercise #1

---
+++ ConstructorExercise/ConstructorExercise/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+//Declares namespace "ConstructorExercise"
+namespace ConstructorExercise
+{
+    //Creates class "Program"
+    class Program
+    {
+        //Program entry point
+        static void Main()
+        {
+            //Requests user input for a number and assigns it to a variable created using keyword "var"
+            Console.WriteLine("Enter a number");
+            var number = Console.ReadLine();
+            //Writes the user's input on the console using the same variable
+            Console.WriteLine("Your input: " + number);
+
+            //Creates string const "word"
+            const string word = "This is a const string";
+            //Writes "word" const on the console
+            Console.WriteLine(word);
+
+            //Prompts user to enter a username and password
+            Console.WriteLine("Register");
+            Console.WriteLine("Enter your username");
+
+            //Creates string "username" with the user input
+            string username = Console.ReadLine();
+
+
+            Console.WriteLine("Create a password");
+
+            //Creates string "username" with the user input
+            string password = Console.ReadLine();
+
+            //Creates "User" instance called "user" with a constructor method and
+            //the "password" string as a parameter
+            User user = new User(password);
+
+            //Writes line on the console displaying the "UserName" and "Password" properties of instance "user"
+            Console.WriteLine(user.UserName + " " + user.Password);
+        }
+    }
+}
---
+++ ConstructorExercise/ConstructorExercise/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Declares namespace "ConstructorExercise"
+namespace ConstructorExercise
+{
+    //Creates class "User"
+    public class User
+    {
+        //Creates "User" properties
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+
+
+        //Calls the other constructor and passes the properties "Person" and password
+        //If no string "username" parameter is given at the time the method is called
+        //then the string "Person" will be passed as a default value
+        public User(string password) : this("Person", password)
+        {
+
+        }
+
+        //All properties are orignally passed
+        public User(string username, string password)
+        {
+            //Object properties are defined with the arguments
+            UserName = username;
+            Password = password;
+
+        }
+
+
+
+    }
+}
