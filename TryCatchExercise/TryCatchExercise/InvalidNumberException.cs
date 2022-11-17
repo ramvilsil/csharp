a91e20963e9dea1455d60909fe62098620123c66
ramvilsil <115331883+ramvilsil@users.noreply.github.com>
2022-11-17 13:15:18 -0800

    Try/Catch Exercise #1

---
+++ TryCatchExercise/TryCatchExercise/InvalidNumberException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+//Declares namespace "TryCatchExercise"
+namespace TryCatchExercise
+{
+    //Creates class for the invalid number exception called "InvalidNumberException" 
+    //inheriting from the "Exception" class
+    public class InvalidNumberException : Exception
+    {
+
+        public InvalidNumberException()
+            : base() { }    
+        public InvalidNumberException(string message) : base(message) { }
+
+
+    }
+}
---
+++ TryCatchExercise/TryCatchExercise/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+//Declares namespace "TryCatchExercise"
+namespace TryCatchExercise
+{
+    //Creates class "Program"
+    class Program
+    {
+        //Program entry point
+        static void Main()
+        {
+            //Creates "DateTime" type instance called "dateTime" and assigns it the "Now" property
+            DateTime dateTime = DateTime.Now;
+
+            //Asks user to enter their age and attempts to assign the value to int "userAge"
+            Console.WriteLine("Enter your age");
+            try
+            {
+                int userAge = Convert.ToInt32(Console.ReadLine());
+                //Subtracts "dateTime"'s year property by the user input assigned to "userAge"
+                dateTime = dateTime.AddYears(-userAge);
+
+                //if the user enters a number less than 0 or greater than 150 
+                //The error message "InvalidNumberException" is thrown
+                if (userAge < 0 || userAge > 150)
+                {
+                    throw new InvalidNumberException();
+                }
+
+                //If the program makes it past the previous if block 
+                //then the following line is written on the console - program used as intended
+                Console.WriteLine("You were born in " + dateTime.Year);
+
+            }
+            
+            //Catches if the user specifically entered an invalid number (from the if block in the try block)
+            catch (InvalidNumberException)
+            {
+                //Notifies the user of the issue and ends the main method
+                Console.WriteLine("You entered an invalid number for your age! - Program terminates here");
+                Console.ReadLine();
+                return;
+            }
+
+            //Catches if the user entered any other invalid input
+            catch (Exception)
+            {
+                //Notifies the user of the issue and ends the main method
+                Console.WriteLine("You entered invalid input! - Program terminates here");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
